a476049f035a0d2d78249d4e87c19936dea22828
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-07-13 12:33:43 -0700

    generate ssl keys

--- config/env/production.js
+++ config/env/production.js
@@ -4,8 +4,8 @@ module.exports = {
 	db: {
 		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 		options: {
-			user: '',
-			pass: ''
+			user: 'admin',
+			pass: 'admin'
 		}
 	},
 	log: {
