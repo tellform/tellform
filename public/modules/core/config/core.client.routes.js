4af0a94c485897eb386d69e057ee84aca6f4157d
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-07-13 18:00:00 -0700

    removed ngclock

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -13,11 +13,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			templateUrl: 'modules/core/views/home.client.view.html'
 		});
 
-
-		$urlRouterProvider.otherwise( function($injector) {
-		  var $state = $injector.get('$state');
-		  $state.go('home');
-		});
+		// $urlRouterProvider.otherwise( function($injector) {
+		//   var $state = $injector.get('$state');
+		//   $state.go('home');
+		// });
 
 	}
 ]);
