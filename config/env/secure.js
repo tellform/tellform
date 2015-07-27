7abf9e92a985fabfed96104fa8f380adec5197c5
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-07-27 12:06:05 -0700

    added mandrill to secure and production configs

--- config/env/secure.js
+++ config/env/secure.js
@@ -62,12 +62,12 @@ module.exports = {
 		callbackURL: 'https://localhost:443/auth/github/callback'
 	},
 	mailer: {
-		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		from: process.env.MAILER_FROM || 'noreply@forms.polydaic.com',
 		options: {
-			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			service: process.env.MAILER_SERVICE_PROVIDER || 'Mandrill',
 			auth: {
-				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+				user: process.env.MAILER_EMAIL_ID || 'hackouver@gmail.com',
+				pass: process.env.MAILER_PASSWORD || 'ehOLuSXCBFxLjipRalBxVg'
 			}
 		}
 	}
