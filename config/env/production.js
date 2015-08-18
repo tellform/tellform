91340fc76086a7d69a545017ac066b7930c16f34
David Baldwynn <david.baldwin@alumni.ubc.ca>
2015-08-18 14:49:45 -0700

    updated mongolab pass

--- config/env/production.js
+++ config/env/production.js
@@ -6,7 +6,7 @@ module.exports = {
 		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 		options: {
 			user: 'admin',
-			pass: 'admin'
+			pass: process.env.MONGOLAB_PASS || 'admin'
 		}
 	},
 	log: {
