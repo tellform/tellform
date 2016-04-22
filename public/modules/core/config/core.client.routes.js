c8d100911e56c1dd0d74c22d5d5379defaccf878
David Baldwynn <david.baldwin@alumni.ubc.ca>
2016-04-22 17:25:50 -0400

    removed landing page

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -4,14 +4,6 @@
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider, Authorization) {
 		// Redirect to home view when route not found
-		$urlRouterProvider.otherwise('/');
-
-		// Home state routing
-		$stateProvider.
-		state('home', {
-			url: '/',
-			templateUrl: 'modules/core/views/home.client.view.html'
-		});
-
+		$urlRouterProvider.otherwise('/#!/forms');
 	}
 ]);
