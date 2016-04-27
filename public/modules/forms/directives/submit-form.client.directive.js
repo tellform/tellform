3694cab7bbbb2ea2a031e04657c2442e3c027e42
Sam <sam@lalop.co>
2016-04-27 18:16:19 +0300

    use $templateCache into submit-form controller

--- public/modules/forms/directives/submit-form.client.directive.js
+++ public/modules/forms/directives/submit-form.client.directive.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('forms').directive('submitFormDirective', ['$http', 'TimeCounter', '$filter', '$rootScope', 'Auth',
-    function ($http, TimeCounter, $filter, $rootScope, Auth) {
+angular.module('forms').directive('submitFormDirective', ['$http', 'TimeCounter', '$filter', '$rootScope', 'Auth', '$templateCache',
+    function ($http, TimeCounter, $filter, $rootScope, Auth, $templateCache) {
         return {
-            templateUrl: 'modules/forms/views/directiveViews/form/submit-form.client.view.html',    
+            template: $templateCache.get('modules/forms/views/directiveViews/form/submit-form.client.view.html'),
             restrict: 'E',
             scope: {
                 myform:'='
@@ -11,14 +11,14 @@ angular.module('forms').directive('submitFormDirective', ['$http', 'TimeCounter'
             controller: function($document, $window, $scope){
                 $scope.authentication = $rootScope.authentication;
 		        $scope.noscroll = false;
-                $scope.forms = {};                
+                $scope.forms = {};
                 $scope.form_fields_count = $scope.myform.visible_form_fields.filter(function(field){
                     if(field.fieldType === 'statement' || field.fieldType === 'rating'){
                         return false;
-                    }	
+                    }
                     return true;
                 }).length;
-		
+
                 $scope.reloadForm = function(){
                     //Reset Form
                     $scope.myform.submitted = false;
@@ -29,24 +29,24 @@ angular.module('forms').directive('submitFormDirective', ['$http', 'TimeCounter'
 
 					$scope.loading = false;
                     $scope.error = '';
-                    
+
                     $scope.selected = {
                         _id: '',
                         index: 0
                     };
-                    $scope.setActiveField($scope.myform.visible_form_fields[0]._id, 0, false);    
-                
+                    $scope.setActiveField($scope.myform.visible_form_fields[0]._id, 0, false);
+
                     console.log($scope.selected);
                     //Reset Timer
-                    TimeCounter.restartClock(); 
+                    TimeCounter.restartClock();
                 };
-				
+
 				$window.onscroll = function(){
             		$scope.scrollPos = document.body.scrollTop || document.documentElement.scrollTop || 0;
 					var elemBox = document.getElementsByClassName('activeField')[0].getBoundingClientRect();
 					$scope.fieldTop = elemBox.top;
 					$scope.fieldBottom = elemBox.bottom;
-			        
+
                     //console.log($scope.forms.myForm);
 
                     if(!$scope.noscroll){
@@ -55,10 +55,10 @@ angular.module('forms').directive('submitFormDirective', ['$http', 'TimeCounter'
                             var field_index = $scope.selected.index+1;
                             var field_id = 'submit_field';
                             $scope.setActiveField(field_id, field_index, false);
-                        } 
+                        }
                         //Focus on field above submit button
                         else if($scope.selected.index === $scope.myform.form_fields.length){
-                            if($scope.fieldTop > 200){    
+                            if($scope.fieldTop > 200){
                                 var field_index = $scope.selected.index-1;
                                 var field_id = $scope.myform.form_fields[field_index]._id;
                                 $scope.setActiveField(field_id, field_index, false);
@@ -70,11 +70,11 @@ angular.module('forms').directive('submitFormDirective', ['$http', 'TimeCounter'
                         }else if ( $scope.selected.index !== 0 && $scope.fieldTop > 0) {
                             var field_index = $scope.selected.index-1;
                             var field_id = $scope.myform.form_fields[field_index]._id;
-                            $scope.setActiveField(field_id, field_index, false); 
+                            $scope.setActiveField(field_id, field_index, false);
                         }
                         //console.log('$scope.selected.index: '+$scope.selected.index);
 					    //console.log('scroll pos: '+$scope.scrollPos+' fieldTop: '+$scope.fieldTop+' fieldBottom: '+$scope.fieldBottom);
-            		    $scope.$apply(); 
+            		    $scope.$apply();
                     }
         		};
 
@@ -127,23 +127,23 @@ angular.module('forms').directive('submitFormDirective', ['$http', 'TimeCounter'
                         $scope.setActiveField(selected_id, selected_index, true);
                     }
                 };
-                
+
                 /*
                 ** Form Display Functions
                 */
                 $scope.exitStartPage = function(){
                     $scope.myform.startPage.showStart = false;
-                    if($scope.myform.form_fields.length > 0){ 
+                    if($scope.myform.form_fields.length > 0){
                         $scope.selected._id = $scope.myform.form_fields[0]._id;
                     }
                 };
 
                 $scope.submitForm = function(){
                     var _timeElapsed = TimeCounter.stopClock();
-					$scope.loading = true;	
+					$scope.loading = true;
                     var form = _.cloneDeep($scope.myform);
                     form.timeElapsed = _timeElapsed;
-                
+
                     form.percentageComplete = $filter('formValidity')($scope.myform)/$scope.myform.visible_form_fields.length*100;
                     delete form.visible_form_fields;
 
