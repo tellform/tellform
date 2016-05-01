abe605590a26d096c068af1ab37d1a026e627e15
Sam <sam@lalop.co>
2016-05-01 12:04:44 +0300

    colorpicker tmp hack

--- public/modules/forms/forms.client.module.js
+++ public/modules/forms/forms.client.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Use Application configuration module to register a new module
-ApplicationConfiguration.registerModule('forms', ['ngFileUpload', 'ui.router.tabs', 'colorpicker.module', 'ui.date', 'ui.sortable', 'angular-input-stars', 'users']);
+ApplicationConfiguration.registerModule('forms', ['ngFileUpload', 'ui.router.tabs', 'ui.date', 'ui.sortable', 'angular-input-stars', 'users']);//, 'colorpicker.module' @TODO reactivate this module
