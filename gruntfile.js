93c4c1ed8412a8b64e21c51cd41dfeea9c660f7f
Sam <sam@lalop.co>
2016-05-01 12:03:55 +0300

    exclude form gruntfile from build

--- gruntfile.js
+++ gruntfile.js
@@ -146,6 +146,14 @@ module.exports = function(grunt) {
 		    	limit: 10
 	    	}
 	    },
+		concurrent: {
+		    default: ['nodemon', 'watch'],
+			debug: ['nodemon', 'watch', 'node-inspector'],
+			options: {
+				logConcurrentOutput: true,
+		    	limit: 10
+	    	}
+	    },
 		env: {
 			test: {
 				NODE_ENV: 'test',
