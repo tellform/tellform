220643de1eaefa46e28b53a2b43b77687305e7f6
David Baldwynn <david.baldwin@alumni.ubc.ca>
2016-05-20 15:08:59 -0700

    fixed multiple choice field

--- public/modules/forms/directives/key-to-option.client.directive.js
+++ public/modules/forms/directives/key-to-option.client.directive.js
@@ -2,26 +2,21 @@
 
 angular.module('forms').directive('keyToOption', function(){
 	return {
-		restrict: 'AE',
-		transclude: true,
+		restrict: 'A',
 		scope: {
-			field: '&'
+			field: '='
 		},
 		link: function($scope, $element, $attrs, $select) {
 			$element.bind('keydown keypress', function(event) {
 
 				var keyCode = event.which || event.keyCode;
 				var index = parseInt(String.fromCharCode(keyCode))-1;
-				console.log($scope.field);
+				//console.log($scope.field);
 
 				if (index < $scope.field.fieldOptions.length) {
 					event.preventDefault();
 					$scope.$apply(function () {
 						$scope.field.fieldValue = $scope.field.fieldOptions[index].option_value;
-						if($attrs.type === 'dropdown'){
-							$select.selected.option_value = $scope.field.fieldOptions[index].option_value;
-						}
-						console.log($scope);
 					});
 				}
 
