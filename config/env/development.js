a966a5f34a9cc4c6c34f5c90eaaa870d4b941e04
Sam <sam@lalop.co>
2016-06-05 01:34:43 +0200

    fix docker

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	baseUrl: process.env.BASE_URL || 'http://localhost:5000',
 	db: {
-		uri: 'mongodb://localhost/mean',
+		uri: 'mongodb://'+(process.env.DB_HOST || 'localhost')+'/mean',
 		options: {
 			user: '',
 			pass: ''
