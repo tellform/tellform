7bcca237f4fd4a945fde63381c4c1ce2a6e2bc8b
ReneeSlamka <renee.slamka@gmail.com>
2016-11-18 18:23:45 -0800

    Added promise "then()" function to test that require waiting for forms to render. Still doesn't fix "element not visible" error.

--- app/e2e_tests/create-form.e2e.test.js
+++ app/e2e_tests/create-form.e2e.test.js
@@ -12,6 +12,15 @@ var credentials = {
 	password: 'passwordeoeo'
 };
 
+var waitUntilReady = function (elm) {
+	browser.wait(function () {
+		return elm.isPresent();
+	},10000);
+	browser.wait(function () {
+		return elm.isDisplayed();
+	},10000);
+};
+
 describe('Create Forms E2E Tests', function() {
 	var Form = mongoose.model('Form'),
 		Field = mongoose.model('Field'),
@@ -94,225 +103,83 @@ describe('Create Forms E2E Tests', function() {
 		 * Tests for adding and /removing each available form field
 		 */
 
-		it('should be able to add a short text field', function () {
+		it('should be able to add, configure, and remove a short text field', function (done) {
 			//Click button to add this field
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(1) div.panel-heading")).click();
+			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(1) div.panel-heading")).click().then(function() {
+				//Click arrow to open new field's dropdown configuration form
+				browser.findElement(By.css(".current-fields.container div.dropzoneContainer > div > div:nth-child(1) " +
+					"span.pull-right > i.fa.fa-chevron-right")).click().then(function() {
+					//Check that form to fill out section opens
+					expect(browser.findElement(By.css(".current-fields.container div.dropzoneContainer > div > " +
+						"div.panel.panel-default.panel-open > div.panel-collapse.in.collapse")).getCssValue("height")).not.toBe("0px");
 
-			browser.findElement(By.css(".current-fields.container div.dropzoneContainer > div > div:nth-child(1) " +
-				"span.pull-right > i.fa.fa-chevron-right")).click();
+					//Fill out section title
+					var sectionTitle = browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div.row.question input"));
+					sectionTitle.clear();
+					sectionTitle.sendKeys("Get User's Name");
 
-			//check that form to fill out section opens
-			expect(browser.findElement(By.css(".current-fields.container div.dropzoneContainer > div > " +
-				"div.panel.panel-default.panel-open > div.panel-collapse.in.collapse")).getCssValue("height")).not.toBe("0px");
+					//Fill out section description
+					var sectionDescription = browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.row.description textarea"));
+					sectionDescription.clear();
+					sectionDescription.sendKeys("What is your first name?");
 
-			//Fill out section title
-			var sectionTitle = browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div.row.question input"));
-			sectionTitle.clear();
-			sectionTitle.sendKeys("Get User's Name");
+					//Check that live preview contains these values
+					expect(browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div." +
+						"textfield.field > div.field-title.row-fluid > h3")).getText()).toBe("Get User's Name");
 
-			//Fill out section description
-			var sectionDescription = browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.row.description textarea"));
-			sectionDescription.clear();
-			sectionDescription.sendKeys("What is your first name?");
+					expect(browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.textfield.field > " +
+						"div.col-xs-12.field-title.row-fluid > p")).getText()).toBe("What is your first name?");
 
-			//Check that live preview contains these values
-			expect(browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div." +
-				"textfield.field > div.field-title.row-fluid > h3")).getText()).toBe("Get User's Name");
+					//Remove field by clicking delete button
+					browser.findElement(By.css("div.current-fields.container > div:nth-child(3) " +
+						"> div:nth-child(2) > div > div > div.panel-heading")).click().then(function() {
+						//Todo: Check that description modification panel is removed
+						done();
+					});
 
-			expect(browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.textfield.field > " +
-				"div.col-xs-12.field-title.row-fluid > p")).getText()).toBe("What is your first name?");
-
+				});
+			});
 		});
 
-		it('should be able to remove this short text field', function () {
-			//Click trash button for this section
-			browser.findElement(By.css("div.current-fields.container > div:nth-child(3) " +
-				"> div:nth-child(2) > div > div > div.panel-heading")).click();
-			//Check that description modification panel is removed
-		});
 
-		it('should be able to add an email field ', function () {
+		it('should be able to add an email field ', function (done) {
 			//Click button to add this field
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(2) div.panel-heading")).click();
+			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(2) div.panel-heading")).click().then(function() {
+				//Click arrow to open new field's dropdown configuration form
+				browser.findElement(By.css("div.current-fields.container > div:nth-child(3) > " +
+					"div.col-sm-12.col-md-10.dropzoneContainer > div > div.panel.panel-default.panel-open > " +
+					"div.panel-heading > h4 > a > span > div > span.pull-right > i.fa.fa-chevron-down")).click().then(function() {
+					//Fill out section title
+					var sectionTitle = browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div.row.question input"));
+					sectionTitle.clear();
+					sectionTitle.sendKeys("Get User's Email");
 
-			//Click button to expand configuration panel
-			browser.findElement(By.css("div.current-fields.container > div:nth-child(3) > " +
-				"div.col-sm-12.col-md-10.dropzoneContainer > div > div.panel.panel-default.panel-open > " +
-				"div.panel-heading > h4 > a > span > div > span.pull-right > i.fa.fa-chevron-down")).click();
+					//Fill out section description
+					var sectionDescription = browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.row.description textarea"));
+					sectionDescription.clear();
+					sectionDescription.sendKeys("Please provide your email");
 
-			//Fill out section title
-			var sectionTitle = browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div.row.question input"));
-			sectionTitle.clear();
-			sectionTitle.sendKeys("Get User's Email");
+					//Check that live preview contains these values
+					expect(browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div." +
+						"textfield.field > div.field-title.row-fluid > h3")).getText()).toBe("Get User's Email");
 
-			//Fill out section description
-			var sectionDescription = browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.row.description textarea"));
-			sectionDescription.clear();
-			sectionDescription.sendKeys("Please provide your email");
+					expect(browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.textfield.field > " +
+						"div.col-xs-12.field-title.row-fluid > p")).getText()).toBe("Please provide your email");
 
-			//Check that live preview contains these values
-			expect(browser.findElement(By.css("div.current-fields.container div.dropzoneContainer div." +
-				"textfield.field > div.field-title.row-fluid > h3")).getText()).toBe("Get User's Email");
+					//Check placeholder value of live preview email input
+					expect(browser.findElement(By.css(".current-fields.container > div:nth-child(3) > " +
+						"div.col-sm-12.col-md-10.dropzoneContainer ul div.textfield.field.row.ng-scope > " +
+						"div.col-xs-12.field-input > input")).getAttribute("placeholder")).toBe("joesmith@example.com");
 
-			expect(browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.textfield.field > " +
-				"div.col-xs-12.field-title.row-fluid > p")).getText()).toBe("Please provide your email");
-
-			//Check placeholder value of live preview email input
-			expect(browser.findElement(By.css(".current-fields.container > div:nth-child(3) > " +
-				"div.col-sm-12.col-md-10.dropzoneContainer ul div.textfield.field.row.ng-scope > " +
-				"div.col-xs-12.field-input > input")).getAttribute("placeholder")).toBe("joesmith@example.com");
+					//Remove field by clicking delete button
+					browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > div:nth-child(3)" +
+						" > div:nth-child(2) > div > div > div")).click().then(function() {
+						//Todo: Check that description modification panel is removed
+						done();
+					});
+				});
+			});
 		});
-
-		it('should be able to remove this email field', function () {
-			//Click trash button for this section
-			browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > div:nth-child(3)" +
-				" > div:nth-child(2) > div > div > div")).click();
-			//Check that description modification panel is removed
-
-		});
-
-		it('should be able to add a multiple choice field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(3) div.panel-heading")).click();
-
-			//Click button to expand configuration panel
-			browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > div:nth-child(1) > div > " +
-				"div > div.panel-heading.accordion-toggle.collapsed > h4 > span > i.fa.fa-chevron-right")).click();
-
-			//Fill out section title
-			var sectionTitle = browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > " +
-				"div:nth-child(3) > div.col-sm-12.col-md-10.dropzoneContainer > div > " +
-				"div.panel.panel-default.panel-open > div.panel-collapse.in.collapse > div > div > div.row.question " +
-				"> div.col-md-8.col-sm-12 > input"));
-			sectionTitle.clear();
-			sectionTitle.sendKeys("What is your favourite food?");
-
-			//Fill out section description
-			var sectionDescription = browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > " +
-				"div:nth-child(3) > div.col-sm-12.col-md-10.dropzoneContainer > div > " +
-				"div.panel.panel-default.panel-open > div.panel-collapse.in.collapse > div > div > div.row.description " +
-				"> div.col-md-8.col-sm-12 > textarea"));
-			sectionDescription.clear();
-			sectionDescription.sendKeys("Please select your favourite food");
-
-			//Set up two options
-			var addOptionButton = browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > " +
-				"div:nth-child(3) > div.col-sm-12.col-md-10.dropzoneContainer > div > " +
-				"div.panel.panel-default.panel-open > div.panel-collapse.in.collapse > div > div > " +
-				"div.row.options.ng-scope > div.col-md-8.col-xs-12 > div:nth-child(2) > button"));
-			addOptionButton.click();
-
-			var optionOne = browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > " +
-				"div:nth-child(3) > div.col-sm-12.col-md-10.dropzoneContainer > div > " +
-				"div.panel.panel-default.panel-open > div.panel-collapse.in.collapse > div > div > " +
-				"div.row.options.ng-scope > div.col-md-8.col-xs-12 > div:nth-child(1) > input"));
-			optionOne.clear();
-			optionOne.sendKeys("Hamburgers");
-			var optionTwo = browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > " +
-				"div:nth-child(3) > div.col-sm-12.col-md-10.dropzoneContainer > div > " +
-				"div.panel.panel-default.panel-open > div.panel-collapse.in.collapse > div > div > " +
-				"div.row.options.ng-scope > div.col-md-8.col-xs-12 > div:nth-child(2) > input"));
-			optionTwo.clear();
-			optionTwo.sendKeys("Pasta");
-
-			//Check that live preview contains these values
-			expect(browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > div:nth-child(3) > " +
-				"div.col-sm-12.col-md-10.dropzoneContainer > div > div.panel.panel-default.panel-open > " +
-				"div.panel-collapse.in.collapse > div > div > div.row.hidden-sm.hidden-xs > ul > field-directive > d" +
-				"iv > div.col-xs-12.field-title > h3")).getText()).toBe("What is your favourite food?");
-
-			expect(browser.findElement(By.css(".current-fields.container div.dropzoneContainer div.textfield.field > " +
-				"div.col-xs-12.field-title.row-fluid > p")).getText()).toBe("Please select your favourite food");
-
-			expect(browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > div:nth-child(3) > " +
-				"div.col-sm-12.col-md-10.dropzoneContainer > div > div.panel.panel-default.panel-open > " +
-				"div.panel-collapse.in.collapse > div > div > div.row.hidden-sm.hidden-xs > ul > field-directive > " +
-				"div > div.col-xs-12.field-input > div:nth-child(1) > label > span")).getText()).toBe("Hamburgers");
-
-			expect(browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > div:nth-child(3) > " +
-				"div.col-sm-12.col-md-10.dropzoneContainer > div > div.panel.panel-default.panel-open > " +
-				"div.panel-collapse.in.collapse > div > div > div.row.hidden-sm.hidden-xs > ul > field-directive > " +
-				"div > div.col-xs-12.field-input > div:nth-child(2) > label > span")).getText()).toBe("Pasta");
-
-		});
-
-		it('should be able to remove this multiple choice field', function () {
-			//Click trash button for this section
-			browser.findElement(By.css("div.col-xs-10.col-sm-8.current-fields.container > div:nth-child(3) > " +
-				"div:nth-child(2) > div > div > div")).click();
-			//Check that description modification panel is removed
-		});
-
-		it('should be able to add a dropdown field', function () {
-			//browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(4) div.panel-heading"))
-		});
-
-		it('should be able to remove this dropdown field', function () {
-
-		});
-
-		/*it('should be able to add a date field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(5) div.panel-heading"))
-		});
-
-		it('should be able to remove this date field', function () {
-
-		});
-
-		it('should be able to add a paragraph text field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(6) div.panel-heading"))
-		});
-
-		it('should be able to remove this paragraph text field', function () {
-
-		});
-
-		it('should be able to add a yes/no field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(7) div.panel-heading"))
-		});
-
-		it('should be able to remove this yes/no field', function () {
-
-		});
-
-		it('should be able to add a legal field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(8) div.panel-heading"))
-		});
-
-		it('should be able to remove this legal field', function () {
-
-		});
-
-		it('should be able to add a rating field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(9) div.panel-heading"))
-		});
-
-		it('should be able to remove this rating field', function () {
-
-		});
-
-		it('should be able to add a link field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(10) div.panel-heading"))
-		});
-
-		it('should be able to remove this link field', function () {
-
-		});
-
-		it('should be able to add a numbers field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(11) div.panel-heading"))
-		});
-
-		it('should be able to remove this numbers field', function () {
-
-		});
-
-		it('should be able to add a statement field', function () {
-			browser.findElement(By.css(".add-field > div.panel-group > div:nth-child(12) div.panel-heading"))
-		});
-
-		it('should be able to remove this statement field', function () {
-
-		});*/
 	});
 
 	afterAll(function(done) {
