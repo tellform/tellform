436d192ca57f72e6aab9fa6e4937f7d329abb92b
David Baldwynn <whitef0x0@users.noreply.github.com>
2017-03-30 10:03:17 -0700

    Fixed mailer env for production config

--- config/env/production.js
+++ config/env/production.js
@@ -54,10 +54,10 @@ module.exports = {
 		from: process.env.MAILER_FROM || 'no-reply@tellform.com',
 		options: {
 			service: process.env.MAILER_SERVICE_PROVIDER || '',
-			ssl: false,
+			/*ssl: false,
 			host: 'smtp.sparkpostmail.com',
 			port: 587,
-			secure: false,
+			secure: false,*/
 			auth: {
 				user: process.env.MAILER_EMAIL_ID || '',
 				pass: process.env.MAILER_PASSWORD || ''
