50b3b381c2703cefa846d2cda9ca33164387a5a0
User <leonard.loo.ks@gmail.com>
2017-08-17 14:06:11 +0800

    Shift sentry code into if development

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 line=$(head -n 1 /etc/hosts)
-line2=$(echo $line | awk '{print $2}')
+# line2=$(echo $line | awk '{print $2}')
 echo "$line forma.sg $(hostname)" >> /etc/hosts
 
 # Restart sendmail
