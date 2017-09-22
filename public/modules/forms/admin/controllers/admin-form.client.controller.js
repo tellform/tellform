7cb12cd0c3f041869d55991fe332baf2c8188a3a
Arshad Ali Abdul Samad <arshadali@Arshads-MacBook-Pro.local>
2017-09-23 00:04:19 +0800

    added error msg to configure screen, msg disappears when it should fade out

--- public/modules/forms/admin/controllers/admin-form.client.controller.js
+++ public/modules/forms/admin/controllers/admin-form.client.controller.js
@@ -10,6 +10,8 @@ angular.module('forms').controller('AdminFormController', ['$rootScope', '$windo
         $scope.myform = myForm;
         $scope.button_clicked  = false;
         $rootScope.saveInProgress = false;
+        $scope.success = $scope.error = null;
+        $scope.show_msg = false;
 
         CurrentForm.setForm($scope.myform);
 
@@ -53,20 +55,34 @@ angular.module('forms').controller('AdminFormController', ['$rootScope', '$windo
                     $rootScope.saveInProgress = true;
                 }
 
-
+                $scope.success = $scope.error = null;
                 if (isDiffed) {
 
                     $scope.updatePromise = $http.put('/forms/' + $scope.myform.admin.agency.shortName +'/' + $scope.myform._id, {changes: data})
                         .then(function (response) {
-
+                            $scope.success = 'Changes Saved!'
                             if (refreshAfterUpdate) $rootScope.myform = $scope.myform = response.data;
 
                         }).catch(function (response) {
                             console.log('Error occured during form UPDATE.\n');
-                            err = response.data;
+                            err = response.data.message;
+                            $scope.error = err 
                         }).finally(function () {
                             // console.log('finished updating');
-                            $scope.button_clicked  = false;
+
+                            window.setTimeout(function() {
+                                $scope.$apply(function() {
+                                    $scope.button_clicked  = false; 
+                                    $scope.show_msg = true
+                                    $scope.startFade = true
+                                    window.setTimeout(function() {
+                                        $scope.$apply(function() {
+                                            $scope.show_msg = false
+                                        });
+                                    }, 2000);
+                                });
+                            }, 2000);
+
                             if (!updateImmediately) {
                                 $rootScope.saveInProgress = false;
                             }
@@ -75,20 +91,35 @@ angular.module('forms').controller('AdminFormController', ['$rootScope', '$windo
                                 return cb(err);
                             }
                         });
+
                 } else {
 
                     var dataToSend = data;
 
                     $scope.updatePromise = $http.put('/forms/' + $scope.myform.admin.agency.shortName + '/' + $scope.myform._id, {form: dataToSend})
                         .then(function (response) {
-
+                            $scope.success = 'Changes Saved!'
                             if (refreshAfterUpdate) $rootScope.myform = $scope.myform = response.data;
 
                         }).catch(function (response) {
                             console.log('Error occured during form UPDATE.\n');
-                            err = response.data;
-                        }).finally(function () {
-                            $scope.button_clicked  = false;
+                            err = response.data.message;
+                            $scope.error = err 
+                        }).finally(function () { 
+
+                            window.setTimeout(function() {
+                                $scope.$apply(function() {
+                                    $scope.button_clicked  = false; 
+                                    $scope.show_msg = true
+                                    $scope.startFade = true
+                                    window.setTimeout(function() {
+                                        $scope.$apply(function() {
+                                            $scope.show_msg = false
+                                        });
+                                    }, 2000);
+                                });
+                            }, 2000);
+                            
                             if (!updateImmediately) {
                                 $rootScope.saveInProgress = false;
                             }
@@ -97,6 +128,7 @@ angular.module('forms').controller('AdminFormController', ['$rootScope', '$windo
                                 return cb(err);
                             }
                         });
+
                 }
             }
         };
