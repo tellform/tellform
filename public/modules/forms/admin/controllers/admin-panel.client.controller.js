e24919ad0139bc462377cd4c9f495b418254646e
Arshad Ali Abdul Samad <arshadali@Arshads-MacBook-Pro.local>
2017-09-29 14:58:50 +0800

    created preliminary modal for agency form entry

--- public/modules/forms/admin/controllers/admin-panel.client.controller.js
+++ public/modules/forms/admin/controllers/admin-panel.client.controller.js
@@ -110,139 +110,90 @@ angular.module('forms').controller('AdminPanelController', ['$scope', '$rootScop
         getPage();
 
         $scope.openEditModal = function(){
-                    $scope.editFieldModal = $uibModal.open({
-                        animation: true,
-                        templateUrl: 'editFieldModal.html',
-                        windowClass: 'edit-modal-window',
-                        controller:  function($uibModalInstance, $scope) {
-              var reader = new FileReader();
-              var curr_field = 'dropdown'
-                            $scope.field = curr_field;
-                            $scope.showLogicJump = false;
+            $scope.editFieldModal = $uibModal.open({
+            animation: true,
+            templateUrl: 'editFieldModal.html',
+            windowClass: 'edit-modal-window',
+                controller:  function($uibModalInstance, $scope) {
+                    var reader = new FileReader();
 
-              // if (curr_field.fieldOptionsFromFile) {
-              //   curr_field.fileOptions = curr_field.fieldOptions;
+                    $scope.loadOptions = function(currField, files) {
+                        if (currField.fieldType === 'dropdown') {
+                            var optionsFile = files[0];
+                            currField.fieldOptionsFile = optionsFile.name;
+                            currField.loadProgress = 0;
 
-              //   curr_field.manualOptions = [];
-              //   curr_field.manualOptions.push('Option 1');
-              // } else {
-              //   curr_field.manualOptions = curr_field.fieldOptions;
-              // }
+                            reader.onload = function(e) {
+                                var fileContent = e.target.result;
+                                var options = fileContent.split('\n').map(option => option.trim());
+                                var uniq_options = [...new Set(options)];
 
-                            // decides whether field options block will be shown
-                            $scope.showAddOptions = function (field){
-                                if(field.fieldType === 'dropdown' || field.fieldType === 'checkbox' || field.fieldType === 'radio'){
-                                    return true;
-                                } else {
-                                    return false;
-                                }
-                            };
+                                currField.fileOptions = [];
 
-                            // add new option to the field
-                            $scope.addOption = function(currField){
-                                if(currField.fieldType === 'checkbox' || currField.fieldType === 'dropdown' || currField.fieldType === 'radio'){
-                                    if (!currField.manualOptions) {
-                                        currField.manualOptions = [];
-                                    }
-
-                                    var lastOptionID = currField.manualOptions.length + 1;
-                                    currField.manualOptions.push('Option ' + lastOptionID);
-                                }
-                            };
-
-                            // delete particular option
-                            $scope.deleteOption = function (currField, option){
-                                if(currField.fieldType === 'checkbox' || currField.fieldType === 'dropdown' || currField.fieldType === 'radio'){
-                                    for (var i = 0; i < currField.manualOptions.length; i++) {
-                                        if (currField.manualOptions[i] === option) {
-                                            currField.manualOptions.splice(i, 1);
-                                            break;
-                                        }
+                                for (let option of uniq_options) {
+                                    if (option) {
+                                        currField.fileOptions.push(option);
                                     }
                                 }
-                            };
 
-              $scope.loadOptions = function(currField, files) {
-                if (currField.fieldType === 'dropdown') {
-                    var optionsFile = files[0];
-                    currField.fieldOptionsFile = optionsFile.name;
-                    currField.loadProgress = 0;
-
-                    reader.onload = function(e) {
-                        var fileContent = e.target.result;
-                        var options = fileContent.split('\n').map(option => option.trim());
-                        var uniq_options = [...new Set(options)];
-
-                        currField.fileOptions = [];
-
-                        for (let option of uniq_options) {
-                            if (option) {
-                                currField.fileOptions.push(option);
+                                var progress = document.querySelector('.load-file-progress');
+                                progress.classList.remove('active');
                             }
+
+                            reader.onprogress = function(e) {
+                                $timeout(function() {
+                                    if (e && e.lengthComputable) {
+                                        currField.loadProgress = Math.round((e.loaded * 100) / e.total);
+                                    }
+                                }, 10);
+                            };
+
+                            reader.readAsText(optionsFile);
                         }
-
-                        var progress = document.querySelector('.load-file-progress');
-                        progress.classList.remove('active');
-                    }
-
-                    reader.onprogress = function(e) {
-                        $timeout(function() {
-                            if (e && e.lengthComputable) {
-                                currField.loadProgress = Math.round((e.loaded * 100) / e.total);
-                            }
-                        }, 10);
                     };
 
-                    reader.readAsText(optionsFile);
-                }
-              };
 
 
-                            // decides whether rating block will be shown
-                            $scope.showRatingOptions = function (field){
-                                if(field.fieldType === 'rating'){
-                                    return true;
-                                } else {
-                                    return false;
-                                }
-                            };
+                    $scope.saveField = function() {
+                        console.log('in save field')
+                        console.log($scope.field)
 
-                            $scope.saveField = function() {
-                                if (curr_field.fieldOptionsFromFile) {
-                                    curr_field.fieldOptions = curr_field.fileOptions;
-                                } else {
-                                    curr_field.fieldOptionsFile = '';
-                                    curr_field.fieldOptions = curr_field.manualOptions;
-                                }
 
-                                // Have to insert back at same spot if it is an edit
-                                var indexToInsert = -1;
-
-                                // Remove duplicate first
-                                if (curr_field.globalId != undefined) {
-                                    for (var i = 0; i < $scope.myform.form_fields.length; i++) {
-                                        var field = $scope.myform.form_fields[i];
-                                        if (field.globalId == curr_field.globalId) {
-                                            $scope.myform.form_fields.splice(i, 1);
-                                            indexToInsert = i;
-                                        }
-                                    }
-                                }
-                                if (indexToInsert >= 0) {
-                                    $scope.myform.form_fields.splice(indexToInsert, 0, curr_field);
-                                } else {
-                                    $scope.myform.form_fields.push(curr_field);
-                                }
-                                $scope.$parent.update(false, $scope.$parent.myform, true, function(){
-                                    $uibModalInstance.close();
-                                });
-
-                            };
-                            $scope.cancel = function(){
-                                $uibModalInstance.close();
-                            };
+                        if (curr_field.fieldOptionsFromFile) {
+                            curr_field.fieldOptions = curr_field.fileOptions;
+                        } else {
+                            curr_field.fieldOptionsFile = '';
+                            curr_field.fieldOptions = curr_field.manualOptions;
                         }
-                    });
-                };
+
+                        // Have to insert back at same spot if it is an edit
+                        var indexToInsert = -1;
+
+                        // Remove duplicate first
+                        if (curr_field.globalId != undefined) {
+                            for (var i = 0; i < $scope.myform.form_fields.length; i++) {
+                                var field = $scope.myform.form_fields[i];
+                                if (field.globalId == curr_field.globalId) {
+                                    $scope.myform.form_fields.splice(i, 1);
+                                    indexToInsert = i;
+                                }
+                            }
+                        }
+                        if (indexToInsert >= 0) {
+                            $scope.myform.form_fields.splice(indexToInsert, 0, curr_field);
+                        } else {
+                            $scope.myform.form_fields.push(curr_field);
+                        }
+                        $scope.$parent.update(false, $scope.$parent.myform, true, function(){
+                            $uibModalInstance.close();
+                        });
+
+                    };
+                    $scope.cancel = function(){
+                        $uibModalInstance.close();
+                    };
+                }
+            });
+        };
     }
 ]);
