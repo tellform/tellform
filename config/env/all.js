8218521a2b1df27d369978a5c91d4d42f568ad05
David Baldwynn <1160417+whitef0x0@users.noreply.github.com>
2017-10-10 15:12:31 -0700

    Update all.js

--- config/env/all.js
+++ config/env/all.js
@@ -8,7 +8,7 @@ module.exports = {
 		keywords:  process.env.APP_KEYWORDS || 'typeform, pdfs, forms, opensource, formbuilder, google forms, nodejs'
 	},
 	db: {
-		uri: 'mongodb://'+ (process.env.DB_PORT_27017_TCP_ADDR || process.env.DB_HOST || 'localhost')+'/mean',
+		uri: 'mongodb://'+ (process.env.DB_PORT_27017_TCP_ADDR || process.env.MONGODB_URI || 'localhost')+'/mean',
 		options: {
 			user: '',
 			pass: ''
