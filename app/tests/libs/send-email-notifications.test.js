b2f2599fa5ee90ab757b4711bfc2d7bfc256b9b0
David Baldwynn <shorerydr10@live.com>
2017-11-02 09:18:41 -0700

    fix css bug for dropdown field in admin view

--- app/tests/libs/send-email-notifications.test.js
+++ app/tests/libs/send-email-notifications.test.js
@@ -10,9 +10,9 @@ var emailNotifications = require('../../libs/send-email-notifications'),
  * Globals
  */
 var validFormFields = [
-	{fieldType:'textfield', title:'First Name', fieldValue: 'John Smith', deletePreserved: false, _id:'56340745f59a6fc9e22028e9'},
-    {fieldType:'link', title:'Your Website',      fieldValue: 'https://johnsmith.me', deletePreserved: false, _id:'5c9e22028e907634f45f59a6'},
-    {fieldType:'number', title:'Your Age',      fieldValue: 45, deletePreserved: false, _id:'56e90745f5934fc9e22028a6'}
+	{fieldType:'textfield', title:'First Name', fieldValue: 'John Smith', deletePreserved: false, globalId:'56340745f59a6fc9e22028e9'},
+    {fieldType:'link', title:'Your Website',      fieldValue: 'https://johnsmith.me', deletePreserved: false, globalId:'5c9e22028e907634f45f59a6'},
+    {fieldType:'number', title:'Your Age',      fieldValue: 45, deletePreserved: false, globalId:'56e90745f5934fc9e22028a6'}
 ];
 
 var validFieldDict = {
