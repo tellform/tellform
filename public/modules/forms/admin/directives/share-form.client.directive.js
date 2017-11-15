69f1f42816bf1a750cbe1317dbad768589129c8c
David Baldwynn <shorerydr10@live.com>
2017-11-15 10:12:01 -0800

    reorganized css for admin-form

--- public/modules/forms/admin/directives/share-form.client.directive.js
+++ public/modules/forms/admin/directives/share-form.client.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('forms').directive('shareFormDirective', ['$rootScope',
-    function ($rootScope) {
+angular.module('forms').directive('shareFormDirective', ['$rootScope', '$translate',
+    function ($rootScope, $translate) {
         return {
             templateUrl: 'modules/forms/admin/views/directiveViews/form/share-form.client.view.html',
             restrict: 'E',
@@ -10,6 +10,13 @@ angular.module('forms').directive('shareFormDirective', ['$rootScope',
             },
             controller: function($scope){
                 $scope.actualFormURL = $scope.actualformurl;
+
+
+                $scope.fullScreen = "<iframe id='iframe' src='{{actualFormURL}}' style='width:100%;height:500px;'></iframe>"+
+                                    "<div style='font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;'>"+
+                                        $translate.instant('POWERED_BY')+
+                                        "<a href='https://www.tellform.com' style='color: #999' target='_blank'>TellForm</a>"+
+                                    "</div>";
             }
         };
     }
