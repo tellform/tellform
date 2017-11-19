35ea6276a78bbd172dac94419e0888bc2f454a57
David Baldwynn <shorerydr10@live.com>
2017-11-19 13:29:12 -0800

    added tests for password controller

--- public/modules/users/controllers/password.client.controller.js
+++ public/modules/users/controllers/password.client.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('users').controller('PasswordController', ['$scope', '$stateParams', '$state', 'User', '$translate', '$window',
-	function($scope, $stateParams, $state, User, $translate, $window) {
+angular.module('users').controller('PasswordController', ['$scope', '$stateParams', '$state', 'User', '$window',
+	function($scope, $stateParams, $state, User, $window) {
 
 		$scope.error = '';
-		$scope.forms = {};
+		if(!$scope.forms) $scope.forms = {};
 
 		// Submit forgotten password account id
 		$scope.askForPasswordReset = function() {
@@ -24,7 +24,7 @@ angular.module('users').controller('PasswordController', ['$scope', '$stateParam
 
 		// Change user password
 		$scope.resetUserPassword = function() {
-			if(!$scope.forms.resetPasswordForm.$invalid){
+			if($scope.forms.hasOwnProperty('resetPasswordForm') && $scope.forms.resetPasswordForm.$valid){
 				$scope.success = $scope.error = null;
 				User.resetPassword($scope.passwordDetails, $stateParams.token).then(
 					function(response){
