88299a2b276c8fa52f43887e7288104c52564f6c
David Baldwynn <shorerydr10@live.com>
2017-11-20 21:21:32 -0800

    simplified form_field property deletion logic in form_submission model

--- app/controllers/helpers.server.controller.js
+++ app/controllers/helpers.server.controller.js
@@ -3,34 +3,33 @@
 const constants = require('../libs/constants');
 
 module.exports = {
-	removeSensitiveModelData: function(type, object){
-		function removeKeysFromDict(dict, keys){
-			for(var i=0; i<keys.length; i++){
-				var curr_key = keys[i];
-				if( dict.hasOwnProperty(curr_key) ){
-					delete dict[curr_key];
-				}
+	removeKeysFromDict: function(dict, keys){
+		for(var i=0; i<keys.length; i++){
+			var curr_key = keys[i];
+			if( dict.hasOwnProperty(curr_key) ){
+				delete dict[curr_key];
 			}
 		}
-
+	},
+	removeSensitiveModelData: function(type, object){
 		switch(type){
 			case 'private_form':
-	            removeKeysFromDict(object, constants.privateFields[type]);
+	            this.this.removeKeysFromDict(object, constants.privateFields[type]);
 	            if(object.admin){
-	            	removeKeysFromDict(object.admin, constants.privateFields.private_user);
+	            	this.removeKeysFromDict(object.admin, constants.privateFields.private_user);
 	            }
 	            break;
 
 	        case 'public_form':
-				removeKeysFromDict(object, constants.privateFields[type]);
+				this.removeKeysFromDict(object, constants.privateFields[type]);
 				if(object.admin){
-	            	removeKeysFromDict(object.admin, constants.privateFields.public_user);
+	            	this.removeKeysFromDict(object.admin, constants.privateFields.public_user);
 	        	}
 	            break;
 
 	        default:
 	        	if(constants.privateFields.hasOwnProperty(type)){
-	        		removeKeysFromDict(object, constants.privateFields[type]);
+	        		this.removeKeysFromDict(object, constants.privateFields[type]);
 	        	} 
 	            break;
 		}
