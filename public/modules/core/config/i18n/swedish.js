c63e905c251bae5e7a126ac909752aedca405eef
Mattias Axell <mattiasaxell@users.noreply.github.com>
2018-02-28 16:31:35 +0100

    Create swedish.js - Swedish translation

---
+++ public/modules/core/config/i18n/swedish.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('core').config(['$translateProvider', function ($translateProvider) {
+
+	$translateProvider.translations('en', {
+		MENU: 'MENY',
+		SIGNUP_TAB: 'Registrera konto',
+		SIGNIN_TAB: 'Logga In',
+		SIGNOUT_TAB: 'Logga Ut',
+		EDIT_PROFILE: 'Redigera Profil',
+		MY_SETTINGS: 'Mina Inställningar',
+		CHANGE_PASSWORD: 'Byt Lösenord',
+		TOGGLE_NAVIGATION: 'Växla navigation'
+	});
+
+}]);
